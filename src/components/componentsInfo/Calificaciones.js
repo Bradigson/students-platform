790a82663003c69ed87a09e3112bb358d7319720
Bradigson <71954800+Bradigson@users.noreply.github.com>
2022-04-14 18:50:44 -0400

    Calification added

--- src/components/componentsInfo/Calificaciones.js
+++ src/components/componentsInfo/Calificaciones.js
@@ -1,8 +1,24 @@
+import '../../assets/styles/Calificacion.scss';
 const Calificaciones = (props)=>{
     return(
-        <div>
-            {props.name}
-        </div>
+       <div className="calificacion">
+            <div className="calificacion__header pb-1">
+                <h2>Calificacion</h2>
+                <span className='text-muted'>Bradigson Hernandez</span>
+            </div>
+
+
+
+            <div className='calificacion__score'>
+                <div className='calificacion-score__point'>
+                    <h2>20</h2>
+                </div>
+                <div className='calificacion-score__comment mt-4'>
+                    <h3>Excelente</h3>
+                </div>
+
+            </div>
+       </div>
     )
 }
 
