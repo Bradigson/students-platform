678baf4a304db2523812fac45f2e9055119619a0
Bradigson <71954800+Bradigson@users.noreply.github.com>
2022-04-14 17:01:24 -0400

    Practica added

--- src/components/componentsInfo/Practicas.js
+++ src/components/componentsInfo/Practicas.js
@@ -1,7 +1,104 @@
+import '../../assets/styles/Practica.scss';
 const Practicas = (props)=>{
     return(
-        <div>
-            {props.name}
+        <div className="practica">
+            <div className="practica__header">
+                <h1>Practicas</h1>
+                <span className='text-muted'>Bradigson Hernandez</span>
+            </div>
+
+            <div className='practicas__descripcion'>
+                <div className="alert alert-primary d-flex align-items-center" role="alert">
+                    <div className='practicas-descripcion__title'>
+                        Intervalos
+                    </div>
+                    <div>
+                        <i className='bx bx-check'></i>
+
+                        <a href='#'><i className='bx bx-chevron-right'></i></a>
+                    </div>
+                    
+                </div>
+
+                <div className="alert alert-success d-flex align-items-center" role="alert">
+                    <div className='practicas-descripcion__title'>
+                        Escalas
+                    </div>
+                    <div>
+                        <i className='bx bx-check'></i>
+
+                        <a href='#'><i className='bx bx-chevron-right'></i></a>
+                    </div>
+                    
+                </div>
+
+
+                <div className="alert alert-warning d-flex align-items-center" role="alert">
+                    <div className='practicas-descripcion__title'>
+                        Acordes
+                    </div>
+                    <div>
+                        <i className='bx bx-check'></i>
+
+                        <a href='#'><i className='bx bx-chevron-right'></i></a>
+                    </div>
+                    
+                </div>
+
+
+                <div className="alert alert-danger d-flex align-items-center" role="alert">
+                    <div className='practicas-descripcion__title'>
+                        Nostas
+                    </div>
+                    <div>
+                        <i className='bx bx-check'></i>
+
+                        <a href='#'><i className='bx bx-chevron-right'></i></a>
+                    </div>
+                    
+                </div>
+
+
+                <div className="alert alert-primary d-flex align-items-center" role="alert">
+                    <div className='practicas-descripcion__title'>
+                        Sostenidos
+                    </div>
+                    <div>
+                        <i className='bx bx-check'></i>
+
+                        <a href='#'><i className='bx bx-chevron-right'></i></a>
+                    </div>
+                    
+                </div>
+
+
+                <div className="alert alert-success d-flex align-items-center" role="alert">
+                    <div className='practicas-descripcion__title'>
+                        Bemoles
+                    </div>
+                    <div>
+                        <i className='bx bx-check'></i>
+
+                        <a href='#'><i className='bx bx-chevron-right'></i></a>
+                    </div>
+                    
+                </div>
+
+
+                <div className="alert alert-warning d-flex align-items-center" role="alert">
+                    <div className='practicas-descripcion__title'>
+                         Teorico
+                    </div>
+                    <div>
+                        <i className='bx bx-check'></i>
+
+                        <a href='#'><i className='bx bx-chevron-right'></i></a>
+                    </div>
+                    
+                </div>
+
+                
+            </div>
         </div>
     )
 }
